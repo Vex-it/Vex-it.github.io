786094a3dd0663b792b660b70a02647e694fcdef
Justin Grierson <ju3tin@hotmail.co.uk>
2018-07-22 03:26:20 -0700

    sdfsfd

--- test4.php
+++ test4.php
@@ -20,7 +20,10 @@
     url: "https://www.theyworkforyou.com/api/getMP?postcode=IG3+8PY&output=js&key=BkHPEZAtrgNGBMmPzJGZgMqa",
     dataType: 'json',
     success: function(data) {
-      alert("hi my name is: " + data.full_name);
+     /* alert("hi my name is: " + data.full_name); */
+        
+        document.write("hello" + data.full_name);
+
     },
     error: function() {
       alert("error");
