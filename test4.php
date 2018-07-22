c5da3cc24433f1a172479bfcfa8cdd48d1429291
Justin Grierson <ju3tin@hotmail.co.uk>
2018-07-22 03:19:42 -0700

    sdfsfd

---
+++ test4.php
@@ -0,0 +1,35 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+    
+    
+    
+    
+    <head>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <title>HTML source of <?php echo $url; ?></title>
+  </head>
+    
+<body>
+<script>$(document).ready(function(){
+  $.ajax({
+    url: "https://www.theyworkforyou.com/api/getMP?postcode=IG3+8PY&output=js&key=BkHPEZAtrgNGBMmPzJGZgMqa",
+    dataType: 'json',
+    success: function(data) {
+      alert("hi my name is: " + data.full_name);
+    },
+    error: function() {
+      alert("error");
+    }
+  });        
+});
+    
+    </script>
+
+
+</body>
+</html>
