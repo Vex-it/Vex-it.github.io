cffac256de6e89490fa4037b45fe72e9895e1d78
Justin Grierson <ju3tin@hotmail.co.uk>
2018-07-22 07:50:33 -0700

    sadaasdasda

--- polling/online_voting/connection.php
+++ polling/online_voting/connection.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(1);
-mysql_connect('localhost', 'root', '') or die(mysql_error());
-mysql_select_db('poll') or die(mysql_error());
+mysqli_connect('eu-cdbr-west-02.cleardb.net', 'b4a860a22398a3', 'dd77175d') or die(mysql_error());
+mysqli_select_db('heroku_a68dd1418d68110') or die(mysql_error());
 
-?>
+ ?>
