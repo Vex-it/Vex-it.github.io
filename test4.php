ddab81ee1d64c83695095c4f8f56f882aab9b136
Justin Grierson <ju3tin@hotmail.co.uk>
2018-07-22 04:50:17 -0700

    sdfsfd

--- test4.php
+++ test4.php
@@ -19,7 +19,7 @@
     <!--<?php $_GET['link']; ?>-->
 <script>$(document).ready(function(){
   $.ajax({
-    url: "https://www.theyworkforyou.com/api/getMP?postcode= <?php echo $_GET["pc"]; ?>&output=js&key=BkHPEZAtrgNGBMmPzJGZgMqa",
+    url: "https://www.theyworkforyou.com/api/getMP?postcode=<?php echo $_GET["pc"]; ?>&output=js&key=BkHPEZAtrgNGBMmPzJGZgMqa",
     dataType: 'json',
     success: function(data) {
      /* alert("hi my name is: " + data.full_name); */
