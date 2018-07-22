de9c92399d750829d8b33713f12b480d9e94d994
Justin Grierson <ju3tin@hotmail.co.uk>
2018-07-22 02:40:54 -0700

    sdfsfd

---
+++ test1.php
@@ -0,0 +1,17 @@
+
+
+<?php
+
+  $url = 'https://www.theyworkforyou.com/api/getMP?postcode=IG3+8PY&output=js&key=BkHPEZAtrgNGBMmPzJGZgMqa';
+
+?>
+<html>
+  <head>
+    <title>HTML source of <?php echo $url; ?></title>
+  </head>
+  <body>
+    <pre>
+<?php echo htmlspecialchars(file_get_contents($url)); ?>
+    </pre>
+  </body>
+</html>
